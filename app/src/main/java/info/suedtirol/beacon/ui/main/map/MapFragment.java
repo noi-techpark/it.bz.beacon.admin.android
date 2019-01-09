a6070fcc242e17df20c9fb8936e23c9ee5b901a4
Johannes Andrich <johannes.andrich@raiffeisen.net>
2019-01-09 16:00:23 +0100

    Add layout for beacon details

--- app/src/main/java/info/suedtirol/beacon/ui/main/map/MapFragment.java
+++ app/src/main/java/info/suedtirol/beacon/ui/main/map/MapFragment.java
@@ -14,9 +14,6 @@ public class MapFragment extends SupportMapFragment {
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
         View view = super.onCreateView(layoutInflater, viewGroup, bundle);
-        if (view != null && getContext() != null) {
-            view.setBackground(getContext().getDrawable(R.drawable.background_top_rounded));
-        }
         return view;
     }
 }
